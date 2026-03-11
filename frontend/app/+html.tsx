45aef9bf17eae8919c5dd3a1a011c0ed8df0eeb6
Aviator 2277 <91645065+Aviator2276@users.noreply.github.com>
2026-03-11 02:53:55 -0400

    Fix background colors

--- frontend/app/+html.tsx
+++ frontend/app/+html.tsx
@@ -13,8 +13,12 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta
           name='viewport'
-          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover'
         />
+        <meta name='theme-color' content='#000000' media='(prefers-color-scheme: dark)' />
+        <meta name='theme-color' content='#ffffff' media='(prefers-color-scheme: light)' />
+        <meta name='apple-mobile-web-app-capable' content='yes' />
+        <meta name='apple-mobile-web-app-status-bar-style' content='black-translucent' />
         <title>VScout</title>
 
         <link rel='manifest' href='/manifest.json' />
